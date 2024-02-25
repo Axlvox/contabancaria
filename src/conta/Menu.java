40170d3ecd4fb1347df14cf3b103fe01c6aafc2b
José Leandro <leandro.jlds@gmail.com>
2024-02-25 03:00:15 -0300

    Criação da classe contaController

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -2,33 +2,36 @@ package conta;
 
 import java.util.Scanner;
 
+import conta.controller.ContaController;
 import conta.model.ContaCorrente;
 import conta.model.ContaPoupanca;
 
 public class Menu {
 
 	public static Scanner leia = new Scanner(System.in);
-	
+
 	public static void main(String[] args) {
+		
+		ContaController contas = new ContaController();
 
 		int opcao;
 
-		ContaCorrente cc1 = new ContaCorrente(1, 123, 1, "José da Silva", 0.0f, 1000.0f);
-		cc1.visualizar();
-		cc1.sacar(12000.0f);
-		cc1.visualizar();
-		cc1.depositar(5000.0f);
-		cc1.visualizar();
-		
-		ContaPoupanca cp1 = new ContaPoupanca(2, 123, 2, "Maria dos Santos", 100000.0f, 15);
-		cp1.visualizar();
-        cp1.sacar(1000.0f);
-		cp1.visualizar();
-		cp1.depositar(5000.0f);
-		cp1.visualizar();
-		
+//		ContaCorrente cc1 = new ContaCorrente(1, 123, 1, "José da Silva", 0.0f, 1000.0f);
+//		cc1.visualizar();
+//		cc1.sacar(12000.0f);
+//		cc1.visualizar();
+//		cc1.depositar(5000.0f);
+//		cc1.visualizar();
+//		
+//		ContaPoupanca cp1 = new ContaPoupanca(2, 123, 2, "Maria dos Santos", 100000.0f, 15);
+//		cp1.visualizar();
+//        cp1.sacar(1000.0f);
+//		cp1.visualizar();
+//		cp1.depositar(5000.0f);
+//		cp1.visualizar();
+
 		while (true) {
-			
+
 			System.out.println("*****************************************************");
 			System.out.println("                                                     ");
 			System.out.println("                BANCO DO BRAZIL COM Z                ");
@@ -44,56 +47,57 @@ public class Menu {
 			System.out.println("            7 - Depositar                            ");
 			System.out.println("            8 - Transferir valores entre Contas      ");
 			System.out.println("            9 - Sair                                 ");
-			System.out.println("                                                     "); 
+			System.out.println("                                                     ");
 			System.out.println("*****************************************************");
 			System.out.println("Entre com a opção desejada:                          ");
 			System.out.println("                                                     ");
 			opcao = leia.nextInt();
-			
-			if(opcao == 9) {
+
+			if (opcao == 9) {
 				System.out.println("\nBanco do Brazil com Z - O seu futuro começa aqui!");
 				leia.close();
 				System.exit(0);
 			}
-			
-			switch(opcao) {
+
+			switch (opcao) {
 			case 1:
 				System.out.println("\n Criar Conta");
-				
-                 break;
+
+				break;
 			case 2:
 				System.out.println("\n Listar todas as Contas");
-				
-                 break;
+				contas.listarTodas();
+
+				break;
 			case 3:
 				System.out.println("\n Buscar Conta por número");
-				
+
 				break;
 			case 4:
 				System.out.println("\n Atualizar dados da Conta");
-				
-                 break;
+
+				break;
 			case 5:
 				System.out.println("\n Apagar Conta");
-				
-                 break;
+
+				break;
 			case 6:
 				System.out.println("\n Sacar");
-				
+
 				break;
-             case 7:
+			case 7:
 				System.out.println("\n Depositar");
-				
+
 				break;
-             case 8:
+			case 8:
 				System.out.println("\n Transferir");
-				
+
 				break;
 			default:
 				System.out.println("\nOpção Inválida");
-                 break;
+				break;
 			}
-        }
+		}
 	}
 
 	public static void sobre() {
